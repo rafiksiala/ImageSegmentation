100fb8ab71a22672e9e92e3141a6769dfdd92a78
rafiksiala <siala.rafik@gmail.com>
2025-05-06 16:01:20 +0200

    Add Mask2Former model

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ from tensorflow.keras.optimizers import Adam
 from keras.losses import CategoricalCrossentropy
 from keras import backend as K
 
+from transformers import Mask2FormerForUniversalSegmentation, AutoImageProcessor
+import torch
+
 
 # ------------------------------------------------------------------------------
 # Mapping des 34 classes → 8 grandes catégories
@@ -36,18 +39,35 @@ cats = {
 }
 cat_mapping = {i: idx for idx, (_, ids) in enumerate(cats.items()) for i in ids}
 
-cityscapes_colors = [
-    (0, 0, 0),          # void
-    (180, 180, 180),    # flat
-    (200, 100, 100),    # construction
-    (100, 40, 40),      # object
-    (107, 142, 35),     # nature
-    (70, 130, 180),     # sky
-    (220, 20, 60),      # human
-    (0, 0, 142),        # vehicle
+EIGHT_CLASS_COLORS = [
+    (0, 0, 0),           # 0 - void
+    (128, 64, 128),      # 1 - flat
+    (70, 70, 70),        # 2 - construction
+    (255, 165, 0),       # 3 - object
+    (107, 142, 35),      # 4 - nature
+    (70, 130, 180),      # 5 - sky
+    (220, 20, 60),       # 6 - human
+    (0, 0, 142)          # 7 - vehicle
 ]
 class_names = ['void', 'flat', 'construction', 'object', 'nature', 'sky', 'human', 'vehicle']
-cmap = ListedColormap(np.array(cityscapes_colors) / 255.0)
+cmap = ListedColormap(np.array(EIGHT_CLASS_COLORS) / 255.0)
+
+
+CITYSCAPES_TO_8_CLASSES = {
+    0: 1, 1: 1,
+    2: 2, 3: 2, 4: 2,
+    5: 3, 6: 3, 7: 3,
+    8: 4, 9: 4,
+    10: 5,
+    11: 6, 12: 6,
+    13: 7, 14: 7, 15: 7, 16: 7, 17: 7, 18: 7
+}
+def remap_cityscapes_to_8classes(mask: np.ndarray) -> np.ndarray:
+    remapped = np.full_like(mask, fill_value=255)
+    for train_id, new_id in CITYSCAPES_TO_8_CLASSES.items():
+        remapped[mask == train_id] = new_id
+    return remapped
+
 
 # ------------------------------------------------------------------------------
 
@@ -166,17 +186,17 @@ def total_loss(y_true, y_pred):
 
 # ------------------------------------------------------------------------------
 
-MODEL_URL = "https://imgsegmodelstorage.blob.core.windows.net/models/dilatednet.keras"
-MODEL_PATH = "models/dilatednet.keras"
+DILATEDNET_URL = "https://imgsegmodelstorage.blob.core.windows.net/models/dilatednet.keras"
+DILATEDNET_PATH = "models/dilatednet.keras"
 
-def download_model_if_needed():
-    os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
+def download_delatednet_model_if_needed():
+    os.makedirs(os.path.dirname(DILATEDNET_PATH), exist_ok=True)
 
-    if not os.path.exists(MODEL_PATH):
+    if not os.path.exists(DILATEDNET_PATH):
         print("Téléchargement du modèle depuis Azure Blob Storage...")
-        with requests.get(MODEL_URL, stream=True) as r:
+        with requests.get(DILATEDNET_URL, stream=True) as r:
             r.raise_for_status()
-            with open(MODEL_PATH, "wb") as f:
+            with open(DILATEDNET_PATH, "wb") as f:
                 for chunk in r.iter_content(chunk_size=8192):
                     f.write(chunk)
         print("Modèle téléchargé avec succès !")
@@ -184,11 +204,34 @@ def download_model_if_needed():
         print("Modèle déjà présent.")
 
 # Télécharger le modèle si nécessaire
-download_model_if_needed()
+download_delatednet_model_if_needed()
 
 # Charger le modèle
-model = load_model(MODEL_PATH, compile=False)
-model.compile(optimizer=Adam(1e-4), loss=total_loss, metrics=[dice_coeff, 'accuracy'])
+
+def load_model(model_name):
+    if model_name == "dilatednet":
+        model = tf.keras.models.load_model(DILATEDNET_PATH, compile=False)
+        model.compile(optimizer=Adam(1e-4), loss=total_loss, metrics=[dice_coeff, 'accuracy'])
+        return model
+    elif model_name == "mask2former":
+        model_id = "facebook/mask2former-swin-small-cityscapes-semantic"
+        return Mask2FormerForUniversalSegmentation.from_pretrained(model_id)
+
+
+def load_processor(model_name):
+    if model_name == "mask2former":
+        model_id = "facebook/mask2former-swin-small-cityscapes-semantic"
+        processor = AutoImageProcessor.from_pretrained(model_id)
+        return processor
+
+model_names = ['dilatednet', 'mask2former']
+models = {}
+processors = {}
+
+for model_name in model_names:
+    models[model_name] = load_model(model_name)
+    if model_name == "mask2former":
+        processors[model_name] = load_processor(model_name)
 
 # ------------------------------------------------------------------------------
 
@@ -205,23 +248,65 @@ def preprocess_image_from_path(path: str):
     img_array = image.img_to_array(pil_img) / 255.0
     return np.expand_dims(img_array, axis=0)
 
-def predict_mask(img_array):
-    prediction = model.predict(img_array)
-    reshaped = prediction[0].reshape((img_height, img_width, n_classes))
-    predicted_mask = np.argmax(reshaped, axis=-1)
-    return predicted_mask
+def predict_mask(image_input, model_name: str = "dilatednet", from_file: bool = True):
+    model = models.get(model_name)
+    if model is None:
+        raise ValueError(f"Unknown model: {model_name}")
+
+    if model_name == "dilatednet":
+        # Prétraitement pour dilatednet
+        if from_file:
+            img_array = preprocess_image_from_file(image_input)
+        else:
+            img_array = preprocess_image_from_path(image_input)
+
+        prediction = model.predict(img_array)
+        reshaped = prediction[0].reshape((img_height, img_width, n_classes))
+        predicted_mask = np.argmax(reshaped, axis=-1)
+        return predicted_mask
+
+    elif model_name == "mask2former":
+        # Prétraitement pour mask2former
+        if from_file:
+            pil_img = Image.open(io.BytesIO(image_input.file.read())).convert("RGB")
+        else:
+            pil_img = Image.open(image_input).convert("RGB")
+
+        processor = processors.get("mask2former")
+        if processor is None:
+            raise ValueError("Processor for mask2former not loaded.")
+
+        # Resize pour garder une référence à la taille originale
+        target_size = pil_img.size[::-1]  # (H, W)
+        inputs = processor(images=pil_img, return_tensors="pt")
+
+        with torch.no_grad():
+            outputs = model(**inputs)
+
+        # Post-processing pour obtenir les prédictions en trainIds
+        pred_mask = processor.post_process_semantic_segmentation(
+            outputs, target_sizes=[target_size]
+        )[0].cpu().numpy()
+
+        # Remapping → 8 classes
+        pred_remapped = remap_cityscapes_to_8classes(pred_mask)
+
+        # Resize pour correspondre à (img_height, img_width)
+        pred_resized = Image.fromarray(pred_remapped.astype(np.uint8)).resize((img_width, img_height), resample=Image.NEAREST)
+        return np.array(pred_resized)
+
 
 # ------------------------------------------------------------------------------
 
 @app.post("/predict_from_file/")
-async def predict_from_file(file: UploadFile = File(...)):
+async def predict_from_file(file: UploadFile = File(...), model_name: str = "dilatednet"):
     print(f"Image reçue : {file.filename} — type : {file.content_type}")
-    preprocessed_img = preprocess_image_from_file(file)
-    predicted_mask = predict_mask(preprocessed_img)
+    predicted_mask = predict_mask(file, model_name=model_name, from_file=True)
     return JSONResponse(content={"mask": predicted_mask.tolist()})
 
 class PredictRequest(BaseModel):
     image_id: str
+    model_name: Optional[str] = "dilatednet"
 
 @app.post("/predict_from_id/")
 async def predict_from_id(req: PredictRequest):
@@ -229,6 +314,5 @@ async def predict_from_id(req: PredictRequest):
     if not os.path.exists(image_path):
         return JSONResponse(status_code=404, content={"error": "Image not found"})
 
-    preprocessed_img = preprocess_image_from_path(image_path)
-    predicted_mask = predict_mask(preprocessed_img)
+    predicted_mask = predict_mask(image_path, model_name=req.model_name, from_file=False)
     return JSONResponse(content={"mask": predicted_mask.tolist()})
